2c145203a78e8aa2bb7e3b5649063d74a0109eca
arxonic <blasterjaxxx33@gmail.com>
2024-06-16 20:44:10 +0300

    edit redme

--- migrations/3_subscribers.up.sql
+++ migrations/3_subscribers.up.sql
@@ -1,4 +1,4 @@
--- Таблица для хранения информации о мессенджерах и связанных с ними ID
+-- Таблица для хранения информации о мессенджерах и связанных с ними  ID
 CREATE TABLE IF NOT EXISTS subscribes (
     user_id         INTEGER NOT NULL, -- Именинник
     sub_id          INTEGER NOT NULL, -- Подписчик
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS subscribes (
     
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (sub_id) REFERENCES users(id),
-    PRIMARY KEY (user_id, sub_id)
+    PRIMARY KEY (user_id, sub_id) 
 );
