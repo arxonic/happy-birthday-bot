363a18abf859df7d4d93d8dad2e845e132fd9d86
arxonic <blasterjaxxx33@gmail.com>
2024-06-16 20:09:20 +0300

    Subscribers scheme add

---
+++ migrations/3_subscribers.up.sql
@@ -0,0 +1,11 @@
+-- Таблица для хранения информации о мессенджерах и связанных с ними ID
+CREATE TABLE IF NOT EXISTS subscribes (
+    user_id         INTEGER NOT NULL, -- Именинник
+    sub_id          INTEGER NOT NULL, -- Подписчик
+    link            TEXT NOT NULL,
+    expire          DATE NOT NULL,
+    
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (sub_id) REFERENCES users(id),
+    PRIMARY KEY (user_id, sub_id)
+);
